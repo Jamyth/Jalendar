faf5599304d1a98bd9c715feb0e0675755434cd1
Jamyth Luk <jamythluk@jamyth.local>
2021-05-24 14:21:12 +0800

    [FEATURE]: jalendar: added static files

--- src/component/DrawerHeader/DrawerMenu.tsx
+++ src/component/DrawerHeader/DrawerMenu.tsx
@@ -1,17 +1,28 @@
-import { Drawer, DrawerContent, DrawerOverlay } from '@chakra-ui/modal';
+import { Drawer, DrawerBody, DrawerContent, DrawerOverlay, DrawerHeader } from '@chakra-ui/modal';
+import { DrawerFooter } from '@chakra-ui/react';
 import React from 'react';
+import { IoIosCalendar } from 'react-icons/io';
 
 interface Props {
     isOpen: boolean;
     toggle: () => void;
 }
 
+const version = '1.0.0';
+
 export const DrawerMenu = React.memo(({ isOpen, toggle }: Props) => {
     return (
         <Drawer isOpen={isOpen} onClose={toggle} placement="right" size="xs">
             <DrawerOverlay />
-            <DrawerContent maxW="200px" p={4}>
-                blah
+            <DrawerContent maxW="200px" py={2} px={4}>
+                <DrawerHeader p={0} justifyContent="center" alignItem="center" d="flex">
+                    Jalendar
+                    <IoIosCalendar />
+                </DrawerHeader>
+                <DrawerBody></DrawerBody>
+                <DrawerFooter p={0} fontSize="sm">
+                    Version {version}
+                </DrawerFooter>
             </DrawerContent>
         </Drawer>
     );
